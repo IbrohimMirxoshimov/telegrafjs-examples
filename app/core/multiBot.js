8b1bbc2431d367d11a7c8b7cdc50bdf136b980c2
Ibrohim Mirxoshimov <imirxoshimov@gmail.com>
2021-02-26 21:29:14 +0500

    add multi bot

---
+++ app/core/multiBot.js
@@ -0,0 +1,41 @@
+const { Telegraf } = require("telegraf");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+const botsData = [
+  {
+    id: 1,
+    name: "Bot 1",
+    token: "aa11",
+  },
+  {
+    id: 2,
+    name: "Bot 2",
+    token: "bb22",
+  },
+];
+
+// create instances
+const bots = botsData.map((data) => {
+  const bot = new Telegraf(TOKEN);
+  bot.context.data = data;
+  return bot;
+});
+
+// registration handlers
+bots.forEach((bot, index) => {
+  bot.start((ctx) => ctx.reply("Hello, it is " + bot.context.data.name));
+
+  // set webhook
+  bot.telegram.setWebhook("https://{YOUR DOMAIN}/" + bot.context.data.id);
+
+  // Set the bot API endpoint
+  app.use(bot.webhookCallback("/" + bot.context.data.id));
+});
+
+// listen port
+app.listen(3000, () => {
+  console.log("Example app listening on port 3000!");
+});
